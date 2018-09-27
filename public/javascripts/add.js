da71c461b889413a24cdeda33a5a778870e728a1
rolne <rolne1992@gmail.com>
2018-09-27 09:39:36 +0200

    Refactoring, added two file for next features.

--- public/javascripts/add.js
+++ public/javascripts/add.js
@@ -1,14 +1,15 @@
-let theater = {'name': 'név', 'website': 'weboldal'};
+let theater = {'name': 'név', 'country': 'ország', 'town': 'város', 'address': 'cím', 'established': 'alapítva', 'website': 'weboldal'};
 let person = {'name': 'név', 'birth': 'születés', 'death': 'elhalálozott'};
 let performance = {'name': 'név', 'place': 'helyszín'};
 let play = {'name': 'név', 'author': 'író', 'published': 'kiadás'};
-let types = {'theater': theater, 'person': person};
+let types = {'theater': theater, 'person': person, 'performance': performance, 'play': play};
 
 $(document).ready(function () {
     let selectType = $('#select-type');
     let addForm = $('#add-form');
     selectType.change(function () {
-        let formDict = types[$( "#select-type option:selected" ).val()];
+        let selected = $( "#select-type option:selected" ).val();
+        let formDict = types[selected];
         addForm.empty();
         for (let key in formDict){
             let inputField = '<input type="text" id="' + key + '" class="input-field"  name="' + key + '" placeholder="' + formDict[key] + '"><br>';
